b9a5e1299dc8a3af5723fd212ed6b9e1570a3654
habib09 <habib09022@gmail.com>
2017-03-28 17:31:20 +0600

    initial commit

---
+++ app/src/main/java/com/example/habib/myapplication/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.habib.myapplication;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity  {
+    private Button b1,b2,b3,b4;
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayAdapter adapter;
+    private ArrayList<String> list = new ArrayList<>();
+    private ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+
+        b1 = (Button) findViewById(R.id.button);
+        b2=(Button)findViewById(R.id.button2);
+        b3=(Button)findViewById(R.id.button3);
+        b4=(Button)findViewById(R.id.button4);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        lv = (ListView)findViewById(R.id.listView);
+    }
+
+    public void on(View v){
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void off(View v){
+        bluetoothAdapter.disable();
+        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
+    }
+
+
+    public  void visible(View v){
+        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
+    }
+
+
+    public void list(View v){
+        getDiscoverableDevices();
+
+        adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+
+
+        adapter.setNotifyOnChange(true);
+        lv.setAdapter(adapter);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String result=data.getStringExtra("result");
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }
+
+    public void getDiscoverableDevices(){
+        bluetoothAdapter.startDiscovery();
+
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                list.add(device.getName());
+
+                adapter.notifyDataSetChanged();
+
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mReceiver);
+    }
+
+}
